e55606c725266875e9964f172aa26173e26f3963
Pietro Niero Roque <pietro.roque@tgcore.com.br>
2021-10-11 15:00:36 -0300

    Adicionar arquivos de projeto.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using TrinusBank.Views;
+
+namespace TrinusBank.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        public ObservableCollection<TransferViewModel> Transfers { get; set; } = new ObservableCollection<TransferViewModel>();
+        private TransferViewModel _currentTransfer;
+        public TransferViewModel CurrentTranser
+        {
+            get => _currentTransfer;
+            set => Set(ref _currentTransfer, value);
+        }
+        public double? TotalValue { get => Transfers
+                                .Where(i => i.Action == Models.Account.Enums.ActionType.Deposit)
+                                .Sum(i => i.Value) - Transfers
+                                .Where(i => i.Action == Models.Account.Enums.ActionType.Withdrawal)
+                                .Sum(i => i.Value); }
+        public ICommand RegisterCommand { get; set; }
+
+        public void Register()
+        {
+            Transfers.Add(CurrentTranser);
+            CurrentTranser = new TransferViewModel();
+        }
+
+        public MainViewModel()
+        {
+            CurrentTranser = new TransferViewModel();
+            RegisterCommand = new RelayCommand(Register);
+            Transfers.CollectionChanged += Transfers_CollectionChanged;
+            Transfers.Add(new TransferViewModel { Name = "Kelvin", Value = 200.50, Date = new DateTime(2021, 10, 07), Type = Models.Account.Enums.TransferType.DOC });
+        }
+
+        private void Transfers_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            OnPropertyChanged(nameof(TotalValue));
+        }
+    }
+}
---
+++ ViewModels/TransferViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using TrinusBank.Models.Account;
+using TrinusBank.Models.Account.Enums;
+
+namespace TrinusBank.ViewModels
+{
+    public class TransferViewModel : BindableBase
+    {
+        public Transfers Model
+        {
+            get => _model;
+            set
+            {
+                _model = value;
+                OnPropertyChanged(null);
+            }
+        }
+        private Transfers _model;
+        public string Name
+        {
+            get => Model.Name;
+            set
+            {
+                if (Model.Name != value)
+                {
+                    Model.Name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public double? Value
+        {
+            get => Model.Value;
+            set
+            {
+                if (Model.Value != value)
+                {
+                    Model.Value = (double)value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public DateTime Date
+        {
+            get => Model.Date;
+            set
+            {
+                if (Model.Date != value)
+                {
+                    Model.Date = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public TransferType Type
+        {
+            get => Model.Type;
+            set
+            {
+                if (Model.Type != value)
+                {
+                    Model.Type = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ActionType Action
+        {
+            get => Model.Action;
+            set
+            {
+                if (Model.Action != value)
+                {
+                    Model.Action = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public TransferViewModel()
+        {
+            Model = new Transfers();
+            Date = DateTime.Now;
+        }
+
+        public TransferViewModel(Transfers transfers)
+        {
+            if (transfers != null)
+            {
+                Model = transfers;
+            }
+        }
+
+        /*protected override void OnValidation()
+        {
+            if (string.IsNullOrWhiteSpace(Name)) ValidationErrors.Add(nameof(Name), "Preencha o nome");
+            if (Age == null || Age == 0) ValidationErrors.Add(nameof(Age), "Preencha a idade");
+            if (string.IsNullOrWhiteSpace(Ssn)) ValidationErrors.Add(nameof(Ssn), "Preencha o CPF");
+        }*/
+    }
+}
